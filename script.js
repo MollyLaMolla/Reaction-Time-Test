c622cbeefbe80c17b97c6d3d70fbc11a32b6a7cd
molly <113532641+MollyLaMolla@users.noreply.github.com>
2025-01-25 11:23:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const checkboxes = document.querySelectorAll('.round-checkbox');
+    const speedboxes = document.querySelectorAll('.speed-checkbox');
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function(event) {
+            if (this.checked) {
+                checkboxes.forEach(cb => {
+                    if (cb !== this) {
+                        cb.checked = false;
+                    }
+                });
+                if (this.id === 'rounds-5') {
+                    rounds = 5;
+                }
+                if (this.id === 'rounds-10') {
+                    rounds = 10;
+                }
+                if (this.id === 'rounds-unlimited') {
+                    rounds = 99999;
+                }
+            }
+            else{
+                event.preventDefault();
+                this.checked = true;
+            }
+        });
+    });
+
+    speedboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function(event) {
+            if (this.checked) {
+                speedboxes.forEach(cb => {
+                    if (cb !== this) {
+                        cb.checked = false;
+                    }
+                });
+                if (this.id === 'normal') {
+                    maxTimeout = 3500;
+                    minTimeout = 1500;
+                }
+                if (this.id === 'slow') {
+                    maxTimeout = 8500;
+                    minTimeout = 1500;
+                }
+                if (this.id === 'fast') {
+                    maxTimeout = 1000;
+                    minTimeout = 1000;
+                }
+            }
+            else{
+                event.preventDefault();
+                this.checked = true;
+            }
+        });
+    });
+});
